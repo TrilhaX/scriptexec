07d08a14c2a59d26459378f84075e780ceeca22b
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-11-08 10:25:24 -0300

    Update ALS.lua

--- Games/ALS.lua
+++ Games/ALS.lua
@@ -70,6 +70,59 @@ function placeUnit()
     end
 end
 
+function webhook()
+    while getgenv().webhook == true do
+        local discordWebhookUrl = urlwebhook
+
+        -- Construindo o payload para enviar para o webhook
+        local payload = {
+            content = "Tempest Hub",
+            embeds = {
+                {
+                    title = "Account Situation",
+                    description = string.format("Name: %s\nLevel: %s\nYen: %s\nRC: %s\nReputation: %s\nTotal Focus Point: %s\nFocus: %s\nEto Yoshimura KIlls: %s\nKoutarou Amon Kills: %s\nNishiki Nishio Kills: %s\nTouka Kirishima Kills: %s\nGya Sacs: %s\nExperience: %s\n2x Exp Time: %s\nDurability: %s\n\n------------------------------------", name, level, yen, rc, rp, TotalFocusPoints, Focus, EtoYoshimuraKills, KoutarouAmonKills, NishikiNishioKills, ToukaKirishimaKills, GyaSacs, Experience, ExpTime2x, Durability),
+                    color = 10098630,
+                    fields = {
+                        {
+                            name = "Discord",
+                            value = "https://discord.gg/tvQqnYKF7j"
+                        }
+                    },
+                    author = {
+                        name = "ALS INF CASTLE"
+                    },
+                    image = {
+                        url = "https://cdn.discordapp.com/attachments/1060039153494007900/1234607717549740062/Imagem_do_WhatsApp_de_2024-04-22_as_22.38.07_6779880f.jpg?ex=6632ab09&is=66315989&hm=a8e2e1b059838b45701b6959ba8e257413b2a099ef79a8c6bba056669a9f8c4b&"
+                    },
+                    thumbnail = {
+                        url = "https://tr.rbxcdn.com/2a201e67272f350e2478d8fdf1c4d9af/150/150/Image/Webp"
+                    }
+                }
+            }
+        }
+
+        -- Convertendo a tabela para JSON
+        local payloadJson = game:GetService("HttpService"):JSONEncode(payload)
+
+        -- Enviando a requisição POST para o webhook do Discord
+        local response = syn.request({
+            Url = discordWebhookUrl,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = payloadJson
+        })
+
+        if response.Success then
+            print("A")
+        else
+            warn("Erro ao enviar mensagem para o Discord:", response.StatusCode, response.Body)
+        end
+        wait(.5)
+    end
+end
+
 Library:Notify('Place the unit you will use in the first slot', 5)
 
 local Tabs = {
@@ -103,6 +156,32 @@ LeftGroupBox:AddToggle("APU", {
 		getgenv().placeUnit = Value
 		placeUnit()
 	end,
+})
+
+LeftGroupBox:AddInput('WebhookURL', {
+    Default = '',
+    Numeric = false,
+    Finished = false,
+    Placeholder = 'Put ur Webhook URL here',
+    Callback = function(Value)
+        urlwebhook = Value
+    end
+})
+
+LeftGroupBox:AddToggle("Webhook", {
+    Text = "Send Webhook when finish game",
+    Default = false,
+    Callback = function(Value)
+        getgenv().webhook = Value
+        if Value then
+            local uiEndGame = game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("EndGameUI")
+            if uiEndGame and uiEndGame.Visible then
+                webhook()
+            else
+                wait(1)
+            end            
+        end
+    end,
 })
 
 local FrameTimer = tick()
