f8a54815da985db1627aa37cee30192d7014cf9c
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-12-30 01:48:43 -0300

    Update JJI.lua

--- Games/JJI.lua
+++ Games/JJI.lua
@@ -33,7 +33,7 @@ local player = Players.LocalPlayer
 local GuiService = game:GetService("GuiService")
 local VirtualInputManager = game:GetService("VirtualInputManager")
 local TweenService = game:GetService("TweenService")
-local speed = 10000
+local speed = 100
 
 --START OF FUNCTIONS
 function hideUIExec()
